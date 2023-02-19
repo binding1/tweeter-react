605d3c1fbef4e576f570d64f5d8dd5c5ff345fb3
Jaewon Bae <jaey.pro@gmail.com>
2023-02-19 00:29:00 -0500

    first commit of tweeter remade in react

---
+++ react-version/src/components/TweetForm.js
@@ -0,0 +1,33 @@
+function TweetForm() {
+  const placeholderString = "What are you humming about?";
+
+  return (
+    <section className="newtweet">
+      <form
+        method="post"
+        action="/tweets"
+        className="newtweet__form"
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
+        <textarea
+          className="form__textarea"
+          name="text"
+          placeholder={placeholderString}
+        ></textarea>
+        <input
+          type="submit"
+          value="Tweet"
+          className="form__input"
+          onClick={(event) => {
+            event.preventDefault();
+          }}
+        />
+        <span className="form__counter">140</span>
+      </form>
+    </section>
+  );
+}
+
+export default TweetForm;
